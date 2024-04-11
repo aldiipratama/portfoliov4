6d3473fe9c2dd3a198a44c13af6a715244ac4034
aldiipratama <paldi0013@gmail.com>
2024-04-11 11:02:37 +0700

    update

--- src/lib/funfact.ts
+++ src/lib/funfact.ts
@@ -3,11 +3,12 @@ const funfact: string[] = [
   "i often playing games with my friends",
   "i like meat and noodles",
   "i am still a student",
-  "i love swimming and playing guitar",
-  "I don't really like studying and reading books but I like trying new things",
-  "I like to go hiking or climbing",
-  "I like camping to refresh my mind",
-  "I love twilight music",
+  "i like swimming and playing guitar",
+  "i don't really like studying and reading books but I like trying new things",
+  "i like to go hiking or climbing",
+  "i like camping to refresh my mind",
+  "i like twilight music",
+  "i like taking pictures",
 ];
 
 export default funfact;
